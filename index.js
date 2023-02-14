5a82661ccbc91572137a7ad90354c237605eada2
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-14 10:39:46 -0500

    fixed issue regarding parsing request body in post method

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("express");
 const app = express();
 
 const PORT = 3232;
@@ -8,9 +10,20 @@ const PORT = 3232;
 //   origin: "https://example.com",
 // };
 
+// app.use(cors());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { username, password } = req.body;
+  // console.log("this is username: " + username);
+  // console.log("this is password: " + password);
 });
 
 app.listen(PORT, () => {
