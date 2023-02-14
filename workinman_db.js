b1174ffa45ecdc78765709ba840e072681383732
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-14 11:26:37 -0500

    added db and working script for db

--- workinman_db.js
+++ workinman_db.js
@@ -3,26 +3,29 @@ const e = require("express");
 let sqlite3 = require("sqlite3").verbose();
 
 let db = new sqlite3.Database(
-  "./workman_db.db",
+  "./workinkman.db",
   sqlite3.OPEN_READWRITE,
   (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("success");
+    if (err && err.code === "SQLITE_CANTOPEN") {
+      // console.log(err);
+      createDatabase();
+      return;
+    } else if (err) {
+      console.log("getting error " + err);
+      exit(1);
     }
   }
 );
 
-// const createDatabase = () => {
-//   let newdb = new sqlite3.Database("users.db", (err) => {
-//     if (err) {
-//       console.log("Getting error " + err);
-//       exit(1);
-//     }
-//     createTables(newdb);
-//   });
-// };
+const createDatabase = () => {
+  let newdb = new sqlite3.Database("./workinman_db.db", (err) => {
+    if (err) {
+      console.log("Getting error " + err);
+      exit(1);
+    }
+    createTables(newdb);
+  });
+};
 
 const createTables = (newdb) => {
   newdb.exec(
