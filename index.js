52fd47ced5be8bd24b46fc5ecf0d5362394d1268
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-15 10:18:11 -0500

    Added authentication to username & password rough

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const bodyParser = require("body-parser");
 const { urlencoded } = require("express");
 const app = express();
 // import { db, runQueries } from "./workinman_db";
-const { db, runQueries } = require("./workinman_db");
+const {
+  db,
+  runQueries,
+  authenticateUsername,
+  authenticatePassword,
+} = require("./workinman_db");
 
 const PORT = 3232;
 
@@ -24,15 +29,32 @@ app.post("/", (req, res) => {
   console.log(req.body);
 
   const { username, password } = req.body;
-  const queries = [
-    `
-  select username from users where username = '${username}';
-  `,
-    `
-  select password from users where password = '${password}';
-  `,
-  ];
-  runQueries(db, queries);
+  // const queries = [
+  //   `
+  // select username from users where username = '${username}';
+  // `,
+  //   `
+  // select password from users where password = '${password}';
+  // `,
+  // ];
+  const usernameQuery = `select username from users where username = '${username}';`;
+
+  const passwordQuery = `select password from users where password = '${password}';`;
+  // console.log("here right now");
+  // console.log(db);
+  // runQueries(db, queries);
+  // queries.map((q) => authenticate(q));
+  let usernameValue = new Boolean(false);
+  authenticateUsername(db, usernameQuery, usernameValue);
+  console.log(usernameValue);
+  let passwordValue = new Boolean(false);
+  authenticatePassword(db, passwordQuery, passwordValue);
+  console.log(passwordValue);
+  if (usernameValue && passwordValue) {
+    console.log("user is authenticated");
+  } else {
+    console.log("user is missing");
+  }
 });
 
 app.listen(PORT, () => {
